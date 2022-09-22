bc9758909afe367485b1d216ec7c1fb36d329b13
Prapthi <prapthinalige@gmail.com>
2022-09-22 16:24:03 +0530

    initialized uow

--- functions/exposurecalc/index.js
+++ functions/exposurecalc/index.js
@@ -28,12 +28,13 @@
 
     }
 
+    const uow = context.org.dataApi.newUnitOfwork();
     
 
     // Define a record using the RecordForCreate type and providing the Developer Name
     const account = uow.registerUpdate(
     {
-        type: Account,
+        type: "Account",
         fields:{
             Id: borrower,
             TotalDirect: TotalDirect,
